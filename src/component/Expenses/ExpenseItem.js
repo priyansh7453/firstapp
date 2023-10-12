c5aec22ec6e4b41e86aeba51d3d129296855be41
Priyansh kushwaha <priyanshkushwaha10@gmail.com>
2023-10-12 11:43:06 +0530

    Change Amount by 100

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,23 +1,33 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  const [amount, setamount] = useState(props.amount);
+
+  const Changeamt = () => {
+    setamount(amount + 100);
+    console.log(amount);
+  };
+
   const clickHandler = () => {
-    // setTitle("Updated");
-    console.log("click");
+    setTitle("Updated");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={Changeamt}>Change amount</button>
     </Card>
   );
 };
